96ecca553ef2ba16a1aad36fc2e731884a60ccbe
lucaOttaviano <luca.ottaviano.lavoro@gmail.com>
2022-04-07 15:50:16 +0200

    first

---
+++ src/test/java/com/example/esamerubrica/EsameRubricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esamerubrica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameRubricaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
